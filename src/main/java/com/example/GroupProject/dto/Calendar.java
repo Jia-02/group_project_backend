4e85a5445e6beb22d8b56e40bc958ca5f044225b
MSI\jing <jing920119@gmail.com>
2025-11-21 17:13:54 +0800

    Calendar.dto

---
+++ src/main/java/com/example/GroupProject/dto/Calendar.java
@@ -0,0 +1,18 @@
+package com.example.GroupProject.dto;
+
+import java.time.LocalDateTime;
+
+public class Calendar {
+
+	private int calendar_id;
+	
+	private String calendar_title;
+
+	private String calendar_description;
+	
+	
+	private LocalDateTime calendar_start_date;
+	
+	
+	private LocalDateTime calendar_end_date;
+}
