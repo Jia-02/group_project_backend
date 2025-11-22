11a937635984d527c3e663875886d39862d8d2de
kelly <kellylove19971225@gmail.com>
2025-11-22 11:35:01 +0800

    分支測試3

--- src/main/java/com/example/GroupProject/service/ReservationService.java
+++ src/main/java/com/example/GroupProject/service/ReservationService.java
@@ -29,7 +29,7 @@ public class ReservationService {
 					ResCodeMessage.PEOPLE_COUNT_FAILED.getCode(), ResCodeMessage.PEOPLE_COUNT_FAILED.getMessage());
 		}
 
-        // 檢查桌位是否已被預約、使用中?
+        // 檢查桌位是否已被預約、使用中
 	
 
 		// 新增訂位
