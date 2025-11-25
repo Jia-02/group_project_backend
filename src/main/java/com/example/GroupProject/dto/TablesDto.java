6e834890626abc709d2b35390a19217d37ebd566
DESKTOP-9UA1KLR\MaoJia <q9753a97534@gmail.com>
2025-11-25 09:00:51 +0800

    後端新增 table值的限制輸入 與 新增table是否有重複id或覆蓋相同位置

--- src/main/java/com/example/GroupProject/dto/TablesDto.java
+++ src/main/java/com/example/GroupProject/dto/TablesDto.java
@@ -1,15 +1,30 @@
 package com.example.GroupProject.dto;
 
+import com.example.GroupProject.constants.ConstantsMessage;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class TablesDto {
 
+	@NotBlank(message = ConstantsMessage.TABLE_ID_ERROR)
 	private String tableId;
 
+	@NotBlank(message = ConstantsMessage.TABLE_STATUS_ERROR)
 	private String tableStatus;
-
+	
+	@Min(value = 2, message = ConstantsMessage.TABLE_CAPACITY_ERROR)
 	private int tableCapacity;
 
+	// 由於畫布設定暫且為500*500 且桌位大小為20*20 所以 x軸座標不能小於0 大於480
+	@Min(value = 0, message = ConstantsMessage.TABLE_POSITION_ERROR)
+	@Max(value = 480, message = ConstantsMessage.TABLE_POSITION_ERROR)
 	private int tablePositionX;
-
+	
+	//	由於畫布設定暫且為500*500 且桌位大小為20*20 所以 y軸座標不能小於0 大於480
+	@Min(value = 0, message = ConstantsMessage.TABLE_POSITION_ERROR)
+	@Max(value = 480, message = ConstantsMessage.TABLE_POSITION_ERROR)
 	private int tablePositionY;
 
 	public String getTableId() {
--- src/main/java/com/example/GroupProject/service/TablesService.java
+++ src/main/java/com/example/GroupProject/service/TablesService.java
@@ -15,34 +15,71 @@ public class TablesService {
 	@Autowired
 	private TablesDao tableDao;
 
-	
-	//新增桌位
+	// 新增桌位
 	public BasicRes addTable(TablesDto table) {
+		
+
+		for (TablesDto item : tableDao.getTableList()) {
+			// 迴圈檢查存在中的桌位ID是否有與新增的桌位ID重複
+			if (table.getTableId().equalsIgnoreCase(item.getTableId())) {
+				return new BasicRes(ResCodeMessage.TABLE_ID_EXIST.getCode(),
+						ResCodeMessage.TABLE_ID_EXIST.getMessage());
+			}
+			// 迴圈檢查該位置是否已存在桌位
+			if (item.getTablePositionX() >= table.getTablePositionX() && //
+					item.getTablePositionX() <= table.getTablePositionX() + 20) {
+				if (item.getTablePositionY() >= table.getTablePositionY() && //
+						item.getTablePositionY() <= table.getTablePositionY() + 20) {
+					return new BasicRes(ResCodeMessage.TABLE_POSITION_EXIST.getCode(),
+							ResCodeMessage.TABLE_POSITION_EXIST.getMessage());
+				} else if (item.getTablePositionY() <= table.getTablePositionY() && //
+						item.getTablePositionY() + 20 >= table.getTablePositionY()) {
+					return new BasicRes(ResCodeMessage.TABLE_POSITION_EXIST.getCode(),
+							ResCodeMessage.TABLE_POSITION_EXIST.getMessage());
+				}
+			} else if (item.getTablePositionX() <= table.getTablePositionX() && //
+					item.getTablePositionX() + 20 >= table.getTablePositionX()) {
+				if (item.getTablePositionY() >= table.getTablePositionY() && //
+						item.getTablePositionY() <= table.getTablePositionY() + 20) {
+					return new BasicRes(ResCodeMessage.TABLE_POSITION_EXIST.getCode(),
+							ResCodeMessage.TABLE_POSITION_EXIST.getMessage());
+				} else if (item.getTablePositionY() <= table.getTablePositionY() && //
+						item.getTablePositionY() + 20 >= table.getTablePositionY()) {
+					return new BasicRes(ResCodeMessage.TABLE_POSITION_EXIST.getCode(),
+							ResCodeMessage.TABLE_POSITION_EXIST.getMessage());
+				}
+			}
+		}
 
 		tableDao.addTable(table);
 
 		return new BasicRes(ResCodeMessage.SUCCESS.getCode(), ResCodeMessage.SUCCESS.getMessage());
 	}
 
-	//list查詢
+	// list查詢
 	public TableListRes getTableList() {
+		
+		if(tableDao.getTableList().isEmpty()) {
+			return new TableListRes(ResCodeMessage.TABLE_NOT_FOUND.getCode(), ResCodeMessage.TABLE_NOT_FOUND.getMessage());
+		}
+		
 		return new TableListRes(ResCodeMessage.SUCCESS.getCode(), ResCodeMessage.SUCCESS.getMessage(),
 				tableDao.getTableList());
 	}
 
-	//刪除桌位
+	// 刪除桌位
 	public BasicRes delTable(TablesDto table) {
-
+		
 		tableDao.delTableByTableId(table);
 
 		return new BasicRes(ResCodeMessage.SUCCESS.getCode(), ResCodeMessage.SUCCESS.getMessage());
 	}
 
-	//更新桌位
+	// 更新桌位
 	public BasicRes updateTable(TablesDto table) {
-
-		tableDao.updateByTableId(table);
 		
+		tableDao.updateByTableId(table);
+
 		return new BasicRes(ResCodeMessage.SUCCESS.getCode(), ResCodeMessage.SUCCESS.getMessage());
 	}
 
