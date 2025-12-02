2bceccebbd1b775904c9ad28207149683ed4a045
kelly <kellylove19971225@gmail.com>
2025-12-02 09:46:37 +0800

    更新sql

--- src/main/resources/gp_1140818.sql
+++ src/main/resources/gp_1140818.sql
@@ -299,8 +299,9 @@ CREATE TABLE `setting` (
   `setting_name` varchar(100) NOT NULL,
   `setting_detail` varchar(1000) NOT NULL,
   `setting_price` int NOT NULL DEFAULT '0',
-  `setting_note` varchar(300) DEFAULT NULL,
+  `setting_img` varchar(500) NOT NULL,
   `setting_active` tinyint NOT NULL DEFAULT '1',
+  `setting_note` varchar(300) DEFAULT NULL,
   `category_id` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`setting_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -428,4 +429,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-02  9:35:41
+-- Dump completed on 2025-12-02  9:45:58
