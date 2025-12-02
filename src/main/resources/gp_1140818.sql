535e1d330f9ccb60974a5e7963a503f27cdf6efb
DESKTOP-9UA1KLR\MaoJia <q9753a97534@gmail.com>
2025-12-02 17:48:47 +0800

    移除 order_details table中的 order_status欄位

--- src/main/resources/gp_1140818.sql
+++ src/main/resources/gp_1140818.sql
@@ -206,7 +206,6 @@ CREATE TABLE `order_details` (
   `order_details_id` int NOT NULL,
   `options_details` varchar(1000) NOT NULL,
   `order_details_price` int NOT NULL DEFAULT '0',
-  `order_status` varchar(45) NOT NULL,
   `inner_id` varchar(60) DEFAULT '0',
   `take_out_id` varchar(60) DEFAULT '0',
   `setting_id` int DEFAULT '0',
@@ -455,4 +454,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-02 16:07:24
+-- Dump completed on 2025-12-02 17:48:06
