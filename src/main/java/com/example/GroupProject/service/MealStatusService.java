d9e2c474df591fe6fc841c74b40c2d83f1851a7c
DESKTOP-9UA1KLR\MaoJia <q9753a97534@gmail.com>
2025-12-11 16:20:55 +0800

    新增 餐點狀態 controller service

---
+++ src/main/java/com/example/GroupProject/service/MealStatusService.java
@@ -0,0 +1,31 @@
+package com.example.GroupProject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.GroupProject.constants.ResCodeMessage;
+import com.example.GroupProject.dao.MealStatusDao;
+
+import com.example.GroupProject.response.MealStatusRes;
+
+@Service
+public class MealStatusService {
+	
+	@Autowired
+	private MealStatusDao mealStatusDao;
+	
+	@Transactional(rollbackFor = Exception.class)
+	public MealStatusRes getMealStatus(int orderId) {
+		
+		if(orderId < 1) {
+			return new MealStatusRes(ResCodeMessage.ORDERS_ID_ERROR.getCode(), //
+					ResCodeMessage.ORDERS_ID_ERROR.getMessage());
+		}
+		
+		return new MealStatusRes(ResCodeMessage.SUCCESS.getCode(), //
+				ResCodeMessage.SUCCESS.getMessage(),mealStatusDao.getMealStatus(orderId));
+		
+	}
+	
+}
